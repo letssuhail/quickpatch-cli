35d4bd7c469b0399a90975c9e416bc4531e0c313
letssuhail <letssuhail@gmail.com>
2026-05-25 10:08:17 +0530

    Fix upgrade command: remove broken git-based update
    The previous implementation ran git commands from the CLI binary
    directory which has no .git repo at runtime. Replaced with a simple
    message showing current version and upgrade instructions.

--- quickpatch_cli/lib/src/commands/upgrade_command.dart
+++ quickpatch_cli/lib/src/commands/upgrade_command.dart
@@ -1,70 +1,29 @@
-import 'dart:io';
-
-import 'package:args/command_runner.dart';
 import 'package:mason_logger/mason_logger.dart';
 import 'package:quickpatch_cli/src/logging/logging.dart';
 import 'package:quickpatch_cli/src/quickpatch_command.dart';
-import 'package:quickpatch_cli/src/quickpatch_version.dart';
+import 'package:quickpatch_cli/src/version.dart';
 
 /// {@template upgrade_command}
 /// `quickpatch upgrade`
 /// A command which upgrades your copy of QuickPatch.
 /// {@endtemplate}
 class UpgradeCommand extends QuickPatchCommand {
-  /// {@macro upgrade_command}
-  UpgradeCommand();
-
   @override
   String get description => 'Upgrade your copy of QuickPatch.';
 
-  /// Name of the command, exposed for the [CommandRunner].
-  static const String commandName = 'upgrade';
-
   @override
-  String get name => commandName;
+  String get name => 'upgrade';
 
   @override
   Future<int> run() async {
-    final updateCheckProgress = logger.progress('Checking for updates');
-
-    late final String currentVersion;
-    try {
-      currentVersion = await quickpatchVersion.fetchCurrentGitHash();
-    } on ProcessException catch (error) {
-      updateCheckProgress.fail();
-      logger.err('Fetching current version failed: ${error.message}');
-      return ExitCode.software.code;
-    }
-
-    late final String latestVersion;
-    try {
-      latestVersion = await quickpatchVersion.fetchLatestGitHash();
-    } on ProcessException catch (error) {
-      updateCheckProgress.fail();
-      logger.err('Checking for updates failed: ${error.message}');
-      return ExitCode.software.code;
-    }
-
-    updateCheckProgress.complete('Checked for updates');
-
-    final isUpToDate = currentVersion == latestVersion;
-    if (isUpToDate) {
-      logger.info('QuickPatch is already at the latest version.');
-      return ExitCode.success.code;
-    }
-
-    final updateProgress = logger.progress('Updating');
-
-    try {
-      await quickpatchVersion.attemptReset(revision: latestVersion);
-    } on ProcessException catch (error) {
-      updateProgress.fail();
-      logger.err('Updating failed: ${error.message}');
-      return ExitCode.software.code;
-    }
-
-    updateProgress.complete('Updated successfully.');
-
+    logger.info(
+      'Current version: ${lightCyan.wrap(packageVersion)}',
+    );
+    logger.info(
+      '\nTo upgrade, run:\n'
+      '  ${lightCyan.wrap('dart pub global activate quickpatch_cli')}\n'
+      '\nOr visit: ${lightCyan.wrap('https://quickpatch.vercel.app/docs')}',
+    );
     return ExitCode.success.code;
   }
 }
